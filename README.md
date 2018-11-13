f997c68cac836cc646be0418f64a4c37ec543aa7
Evangelos Barakos <e.mparakos@gmail.com>
2018-11-13 17:53:33 +0200

    used async/await, response in sync is rich, added README

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-etherscanner
+# Etherscanner
+
+Store and retrieve Ethereum balances and transactions for certain addresses
+
+
+
+## Install
+
+You need to setup mongoUrl and etherscanAPIkey to an .env file in root folder
+
+`npm install`
+
+`npm start run`
+
+
+
+## API
+
+
+### GET /sync/:address
+
+Store info for an address
+
+Example `/sync/0xAddress`
+
+
+### GET /transactions/
+
+Retrieve stored transactions. 
+
+Optional filter by the following query params: from, to, value, blockNumber 
+
+Example `/transactions?from=0xAddressAto=0xAddressB`
+
+
+### GET /balance/:address
+
+Retrieve balance of an address
+
+Example `/balance/0xAddress`
--- routes/sync.js
+++ routes/sync.js
@@ -11,95 +11,86 @@ const axiosInstance = axios.create({
   baseURL: 'http://api.etherscan.io/api?module=account&sort=asc&',
 });
 
-router.get('/:address', (req, res) => {
-  // TODO: need to move code below into another file?
-  // TODO: maybe use async await? need to resolve all promises before sending back info
-
-  axiosInstance.get(`&action=balance&address=${req.params.address}&apikey=${process.env.etherscanAPIkey}`)
-    .then((response) => {
-      Account
-        .findOneAndUpdate(
-          { _id: req.params.address },
+router.get('/:address', async (req, res) => {
+  const resp = [];
+  try {
+    const response = await axiosInstance.get(`&action=balance&address=${req.params.address}&apikey=${process.env.etherscanAPIkey}`);
+    await Account
+      .findOneAndUpdate(
+        { _id: req.params.address },
+        {
+          $set:
           {
-            $set:
-            {
-              address: req.params.address,
-              balance: response.data.result,
-            },
+            balance: response.data.result,
           },
-          { upsert: true, new: true },
-        )
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((error) => {
-      console.log(error);
+        },
+        { upsert: true, new: true },
+      );
+    resp.push({
+      account: `${req.params.address}`,
+      message: 'stored balance for this account',
     });
-  axiosInstance.get(`&action=txlist&address=${req.params.address}&apikey=${process.env.etherscanAPIkey}`)
-    .then((response) => {
-      console.log(response.data.result);
-      const resp = [];
-      Account
+  } catch (err) {
+    resp.push({
+      error: err,
+    });
+  }
+  try {
+    const response = await axiosInstance.get(`&action=txlist&address=${req.params.address}&apikey=${process.env.etherscanAPIkey}`)
+    try {
+      await Account
         .findOneAndUpdate(
           { _id: req.params.address },
           {
             $set:
-              { address: req.params.address, transactions: response.data.result.map(tx => tx.hash) },
+              { transactions: response.data.result.map(tx => tx.hash) },
           },
           { upsert: true, new: true },
-        )
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((err) => {
-          console.log(err);
+        );
+      resp.push({
+        account: `${req.params.address}`,
+        message: 'stored transactions for this account',
+      });
+    } catch (err) {
+      resp.push({
+        error: err,
+      });
+    }
+    // eslint-disable-next-line no-restricted-syntax
+    for (const tx of response.data.result) {
+      const transaction = new Transaction({
+        _id: tx.hash,
+        from: tx.from,
+        to: tx.to,
+        blockNumber: tx.blockNumber,
+        value: tx.value,
+      });
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        await transaction.save();
+        resp.push({
+          tx: `${tx.hash}`,
+          message: 'stored transaction',
         });
-      // eslint-disable-next-line no-plusplus
-      for (let i = 0; i < response.data.result.length; i++) {
-        const transaction = new Transaction({
-          _id: response.data.result[i].hash,
-          from: response.data.result[i].from,
-          to: response.data.result[i].to,
-          blockNumber: response.data.result[i].blockNumber,
-          value: response.data.result[i].value,
-        });
-        transaction
-          .save()
-          .then((result) => {
-            console.log(result);
-            resp.push({
-              tx_hash: `${response.data.result[i].hash}`,
-              message: 'stored transaction info',
-              storedTransaction: result,
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-            // error code for _id that exists
-            if (err.errmsg.startsWith('E11000')) {
-              resp.push({
-                tx_hash: `${response.data.result[i].hash}`,
-                message: 'already stored',
-              });
-            } else {
-              resp.push({
-                tx_hash: `${response.data.result[i].hash}`,
-                error: err,
-              });
-            }
+      } catch (err) {
+        // error code for _id that exists
+        if (err.errmsg.startsWith('E11000')) {
+          resp.push({
+            tx: `${tx.hash}`,
+            message: 'already stored',
           });
+        } else {
+          resp.push({
+            tx: `${tx.hash}`,
+            error: err,
+          });
+        }
       }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
-  // res.status(200).send(resp);
-  res.status(200).json({ message: 'synced' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(resp);
 });
 
 module.exports = router;
